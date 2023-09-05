bce862aede72801476d63c51b8fa3c83578c35fd
levangelashvili9 <levan.gelashvili9@gmail.com>
2023-09-05 22:59:42 +0400

    made simple test form

--- app/(auth)/(routes)/register/page.tsx
+++ app/(auth)/(routes)/register/page.tsx
@@ -1,9 +1,33 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { registerSchema } from "@/validations";
+import { RegisterFormType } from "@/types";
+
 export default function Register() {
+  const { register, handleSubmit } = useForm<RegisterFormType>({
+    resolver: zodResolver(registerSchema),
+  });
+
+  const onSubmit = (data: RegisterFormType) => {
+    console.log(data);
+  };
+
   return (
     <div>
-      <input type="text" placeholder="sds" />
-      <input type="text" placeholder="sds" />
-      <input type="text" placeholder="sds" />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input type="text" placeholder="sds" {...register("name")} />
+        <input type="text" placeholder="sds" {...register("email")} />
+        <input type="password" placeholder="sds" {...register("password")} />
+        <input
+          type="password"
+          placeholder="sds"
+          {...register("password_confirmation")}
+        />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
